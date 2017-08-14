8a232f784de7fe22283aa72d2a65cf754faf48d7
hyunwoo <hyunwoo9283@gmail.com>
2017-08-14 09:41:55 +0900

    initial lucene sample

---
+++ src/main/java/kr/johnhw/App.java
@@ -0,0 +1,19 @@
+package kr.johnhw;
+
+import org.apache.lucene.index.IndexWriter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+
+        IndexWriter writer;
+
+
+    }
+}
